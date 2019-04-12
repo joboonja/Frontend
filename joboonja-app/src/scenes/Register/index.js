1cfc76cd3be831ce5d96577a52edc5f1d87205e1
Yasaman <ys.jafari98@gmail.com>
2019-04-12 07:59:36 +0430

    add slideshow for signup page

--- joboonja-app/src/scenes/Register/index.js
+++ joboonja-app/src/scenes/Register/index.js
@@ -1,54 +1,63 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// import { NavLink } from 'react-router-dom';
-// import { connect } from 'react-redux';
-// import Card from '../../components/Card';
-// import './styles.scss';
-// import AddButton from '../../components/Buttons/AddButton';
-// import SampleInput from '../../components/Inputs/SimpleInput';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import Card from '../../components/Card';
+import './styles.scss';
+import AddButton from '../../components/Buttons/AddButton';
+import SampleInput from '../../components/Inputs/SimpleInput';
+import SlideShow from './componenets/SlideShow';
+
+class Register extends React.PureComponent {
+  render() {
+    const {
+      username, password, onPasswordChange, onUserChange, valid, userError, passwordError,
+    } = this.props;
+    return (
+      <div>
+        <body className="register-body">
+          <SlideShow />
+        </body>
+        <div className="main container">
+        </div>
+      </div>
+
+    //       <div className="main">
+    //         {/* <SlideShow /> */}
+    //         <div className="loginContainer">
+    //           <Card hoverable className="loginCard">
+    //             <div className="loginTitle">
+    //               <h4>ورود به جاب‌اونجا</h4>
+    //             </div>
+    //             <form noValidate autoComplete="off" className="loginForm">
+    //               <SampleInput className="loginInput" placeholder="نام کاربری" value={username} onChange={onUserChange} error={userError} />
+    //               <SampleInput className="loginInput" placeholder="رمز عبور" type="password" error={passwordError} value={password} onChange={onPasswordChange} />
+    //             </form>
+    //
+    //             <div className="row loginButtonContainer">
+    //               <AddButton width="100px" disabled={!valid}>ورود</AddButton>
+    //             </div>
+    //
+    //             <div className="loginFooter">
+    //               <NavLink className="loginLink"> رمز عبور را فراموش کرده‌ام.</NavLink>
+    //               <NavLink className="loginLink" to="/signup"> ثبت‌نام</NavLink>
+    //             </div>
+    //           </Card>
+    //         </div>
+    //       </div>
+    );
+  }
+}
 //
-// class Register extends React.PureComponent {
-//   render() {
-//     const {
-//       username, password, onPasswordChange, onUserChange, valid, userError, passwordError,
-//     } = this.props;
-//     return (
-//       <div className="main">
-//         {/* <SlideShow /> */}
-//         <div className="loginContainer">
-//           <Card hoverable className="loginCard">
-//             <div className="loginTitle">
-//               <h4>ورود به جاب‌اونجا</h4>
-//             </div>
-//             <form noValidate autoComplete="off" className="loginForm">
-//               <SampleInput className="loginInput" placeholder="نام کاربری" value={username} onChange={onUserChange} error={userError} />
-//               <SampleInput className="loginInput" placeholder="رمز عبور" type="password" error={passwordError} value={password} onChange={onPasswordChange} />
-//             </form>
-//
-//             <div className="row loginButtonContainer">
-//               <AddButton width="100px" disabled={!valid}>ورود</AddButton>
-//             </div>
-//
-//             <div className="loginFooter">
-//               <NavLink className="loginLink"> رمز عبور را فراموش کرده‌ام.</NavLink>
-//               <NavLink className="loginLink" to="/signup"> ثبت‌نام</NavLink>
-//             </div>
-//           </Card>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-//
-// Register.propTypes = {
-//   username: PropTypes.string.isRequired,
-//   password: PropTypes.string.isRequired,
-//   onUserChange: PropTypes.func.isRequired,
-//   onPasswordChange: PropTypes.func.isRequired,
-//   userError: PropTypes.string.isRequired,
-//   passwordError: PropTypes.string.isRequired,
-//   valid: PropTypes.bool.isRequired,
-// };
+Register.propTypes = {
+  username: PropTypes.string.isRequired,
+  password: PropTypes.string.isRequired,
+  onUserChange: PropTypes.func.isRequired,
+  onPasswordChange: PropTypes.func.isRequired,
+  userError: PropTypes.string.isRequired,
+  passwordError: PropTypes.string.isRequired,
+  valid: PropTypes.bool.isRequired,
+};
 //
 // const mapStateToProps = store => ({
 //   username: store.Register.username,
@@ -64,3 +73,4 @@
 // });
 //
 // export default connect(mapStateToProps, mapDispatchToProps)(Register);
+export default Register;
