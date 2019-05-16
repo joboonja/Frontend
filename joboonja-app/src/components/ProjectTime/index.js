d59ed4a4edc6e2b4145783182db06ccb977b9f43
Yasaman <ys.jafari98@gmail.com>
2019-05-16 16:05:54 +0430

    add realtime update for deadline

--- joboonja-app/src/components/ProjectTime/index.js
+++ joboonja-app/src/components/ProjectTime/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-nested-ternary */
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import ProjectSummaryTime from './components/ProjectSummaryTime';
 import convertEnglishNumbersToPersian from '../../services/convertEnglishNumbersToPersian';
@@ -7,29 +7,41 @@ import IconItem from '../IconItem';
 import BidCard from '../../scenes/Projects/components/ProjectInfoBox/components/BidCard';
 
 function ProjectTime({ time, showStyle, id }) {
-  const days = Math.floor((time - Date.now()) / (36e5 * 24));
-  const hours = Math.floor(((time - days * (36e5 * 24)) - Date.now()) / (36e5));
+  const [now, setNow] = useState(new Date());
+  setInterval(() => {
+    setNow(new Date());
+  }, 500);
+  const days = Math.floor((time - now) / (36e5 * 24));
+  const hours = Math.floor(((time - days * (36e5 * 24)) - now) / (36e5));
   const minutes = Math.floor(
     (
-      (time - days * (36e5 * 24) - hours * 36e5) - Date.now()
+      (time - days * (36e5 * 24) - hours * 36e5) - now
     ) / (1000 * 60),
+  );
+  const seconds = Math.floor(
+    (
+      (time - days * (36e5 * 24) - hours * 36e5 - minutes * 1000 * 60) - now
+    ) / (1000),
   );
   const ended = days < 0 || hours < 0 || minutes < 0;
   const daysStr = convertEnglishNumbersToPersian(days.toString());
   const hoursStr = convertEnglishNumbersToPersian(hours.toString());
   const minutesStr = convertEnglishNumbersToPersian(minutes.toString());
+  const secondsStr = convertEnglishNumbersToPersian(seconds.toString());
+
   return (
     <div>
       {
         showStyle === 'winner' ? (ended ? <IconItem type="winner" text="تعیین نشده" textTitle="برنده: " /> : '')
           : showStyle === 'projectCard' ? (ended ? <IconItem type="deadlineReached" text="مهلت تمام شده" textTitle="" />
-            : <IconItem type="deadline" text={`${daysStr} روز و ${hoursStr} ساعت و ${minutesStr} دقیقه`} textTitle="زمان باقی‌مانده:" />)
+            : <IconItem type="deadline" text={`${daysStr} روز و ${hoursStr} ساعت و ${minutesStr} دقیقه و ${secondsStr} ثانیه `} textTitle="زمان باقی‌مانده:" />)
             : showStyle === 'bidCard' ? <BidCard ended={ended} id={id} />
               : (
                 <ProjectSummaryTime
                   days={daysStr}
                   hours={hoursStr}
                   minutes={minutesStr}
+                  seconds={secondsStr}
                   ended={ended}
                 />
               )
